e9a59b43f19278b14848754d66852fcad1c6a9ad
mkoutra <koutra98@hotmail.com>
2024-08-29 00:15:46 +0300

    ChangePasswordFrame implemented not tested.
    - ChangePasswordFrame implemented.
    - ChangePasswordValidator implemeted, but I need to rewrite it.

---
+++ src/main/java/personal/validator/ChangePasswordValidator.java
@@ -0,0 +1,58 @@
+package personal.validator;
+
+import personal.dao.exceptions.UserDAOException;
+import personal.dto.ChangePasswordDTO;
+import personal.service.exceptions.UserNotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ChangePasswordValidator {
+    private List<String> errors = new ArrayList<>();
+
+    public ChangePasswordDTO() {}
+
+    public List<String> validate(ChangePasswordDTO dto) {
+        if (dto == null || dto.getCurrentPassword() == null || dto.getNewPassword() == null
+                || dto.getReEnteredPassword().isEmpty()) {
+            errors.add("Form has not completed yet.");
+            return errors;
+        }
+
+        validatePassword(dto.getNewPassword(), errors);
+
+        if (errors.isEmpty()) {
+            validateReEnteredPassword(dto.getReEnteredPassword(), dto.getNewPassword(), errors);
+        }
+        return errors;
+    }
+
+    private void validatePassword(String password, List<String> errors) {
+        if (password == null) {
+            errors.add("Password null error.");
+            return;
+        }
+
+        Pattern pattern = Pattern.compile(
+                "(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[()#?!@$%^&*\\-])^.{8,20}$");
+        Matcher matcher;
+        matcher = pattern.matcher(password);
+        if (!matcher.find()) {
+            errors.add("Invalid password.");
+        }
+    }
+
+    private void validateReEnteredPassword(String reEnteredPassword, String password, List<String> errors) {
+        if (reEnteredPassword == null || password == null) {
+            errors.add("Re-Entered password null error.");
+            return;
+        }
+
+        if (!reEnteredPassword.equals(password)) {
+            errors.add("Passwords do not much.");
+        }
+    }
+}
--- src/main/java/personal/viewcontroller/ChangePasswordFrame.java
+++ src/main/java/personal/viewcontroller/ChangePasswordFrame.java
@@ -1,29 +1,70 @@
 package personal.viewcontroller;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import personal.dao.IUserDAO;
+import personal.dao.UserDAOImpl;
+import personal.dao.exceptions.UserDAOException;
+import personal.dto.ChangePasswordDTO;
+import personal.dto.UserReadOnlyDTO;
+import personal.model.User;
+import personal.service.IUserService;
+import personal.service.UserServiceImpl;
+import personal.service.exceptions.UserNotFoundException;
+import personal.service.exceptions.WrongPasswordException;
+import personal.validator.ChangePasswordValidator;
+
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.JPasswordField;
 import java.awt.Color;
-import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.List;
 
 public class ChangePasswordFrame extends JFrame {
+	private final IUserDAO userDAO = new UserDAOImpl();
+	private final IUserService userService = new UserServiceImpl(userDAO);
+
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JPasswordField newPasswordField;
 	private JPasswordField reEnteredPasswordField;
 	private JPasswordField currentPasswordField;
+	private User user = null;
 
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
 
 	/**
 	 * Create the frame.
 	 */
 	public ChangePasswordFrame() {
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowOpened(WindowEvent e) {
+				cleanAll();
+			}
+
+			@Override
+			public void windowActivated(WindowEvent e) {
+				cleanAll();
+			}
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				cleanAll();
+				dispose();
+			}
+		});
 		setResizable(false);
 		setTitle("Change Password");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		setBounds(100, 100, 453, 260);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -64,6 +105,39 @@ public class ChangePasswordFrame extends JFrame {
 		panel.add(reEnteredPasswordField);
 		
 		JButton submitBtn = new JButton("Submit");
+		submitBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ChangePasswordDTO changePasswordDTO = createChangePasswordDTO();
+				ChangePasswordValidator validator = new ChangePasswordValidator();
+				List<String> errors = validator.validate(changePasswordDTO);
+				StringBuilder sb = new StringBuilder();
+
+				if (user == null) return;
+
+				if (!errors.isEmpty()) {
+					errors.forEach((error) -> sb.append(error).append('\n'));
+					JOptionPane.showMessageDialog(null, sb.toString(), "Error", JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+
+				try {
+					int response = JOptionPane.showConfirmDialog(null, "Are you sure?",
+							"Confirmation", JOptionPane.YES_NO_OPTION);
+					if (response == JOptionPane.YES_OPTION) {
+						userService.changePassword(user, changePasswordDTO);
+						cleanAll();
+						JOptionPane.showMessageDialog(null, "Password changed successfully",
+								"Password Changed", JOptionPane.INFORMATION_MESSAGE);
+						dispose();
+					}
+				} catch (WrongPasswordException e1) {
+					JOptionPane.showMessageDialog(null, e1.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+				} catch (UserNotFoundException | UserDAOException e2) {
+					JOptionPane.showMessageDialog(null, "Error retrieving the user", "Error", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
 		submitBtn.setForeground(new Color(52, 101, 164));
 		submitBtn.setBounds(120, 116, 117, 25);
 		panel.add(submitBtn);
@@ -78,5 +152,20 @@ public class ChangePasswordFrame extends JFrame {
 		changePasswordLabel.setForeground(new Color(238, 238, 236));
 		changePasswordLabel.setBounds(160, 12, 133, 15);
 		titlePanel.add(changePasswordLabel);
+	}
+
+	private ChangePasswordDTO createChangePasswordDTO() {
+		ChangePasswordDTO dto = new ChangePasswordDTO();
+		dto.setCurrentPassword(new String(currentPasswordField.getPassword()));
+		dto.setNewPassword(new String(newPasswordField.getPassword()));
+		dto.setReEnteredPassword(new String(reEnteredPasswordField.getPassword()));
+
+		return dto;
+	}
+
+	private void cleanAll() {
+		currentPasswordField.setText("");
+		newPasswordField.setText("");
+		reEnteredPasswordField.setText("");
 	}
 }
